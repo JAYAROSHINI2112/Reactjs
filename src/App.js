2b4a6fc9747ce5c54d92ae316cb3077f0faf706e
JAYA ROSHINI <70878001+JAYAROSHINI2112@users.noreply.github.com>
2021-05-22 18:28:59 +0530

    function components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, {Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/greet';
+import WelcomeC from './components/welcome'
        class App extends Component {
        render(){
          return(
            <div className="App">
            <Greet />
+           <WelcomeC />
            </div>
          );
        }
---
+++ src/components/welcome.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react'
+
+class welcome extends Component{
+    render(){
+        return<h2>Learing components</h2>
+    }
+}
+export default welcome
